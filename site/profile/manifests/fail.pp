ca43b429d22f1710eed87a457e3e49ed8780c65d
Abir Majumdar <beersy@gmail.com>
2019-12-04 12:42:59 -0500

    Create fail.pp

---
+++ site/profile/manifests/fail.pp
@@ -0,0 +1,9 @@
+# @summary This profile causes an intentional failed puppet run
+class profile::fail {
+
+  file { 'Create a nested folder'
+    path   => '/opt/puppetlabs/this/nested/path/doesnt/exist'
+    ensure => 'directory'
+  }
+  
+}
